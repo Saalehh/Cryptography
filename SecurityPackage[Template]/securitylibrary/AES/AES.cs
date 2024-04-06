b9e23921a9ec858341f1e2576138e0b092b8f185
Abdulrhman Hosny <abdulrhmanhosnymuhammed@gmail.com>
2024-04-06 04:13:04 +0200

    Finished AES Decryption and Modified Encryption

--- SecurityPackage[Template]/securitylibrary/AES/AES.cs
+++ SecurityPackage[Template]/securitylibrary/AES/AES.cs
@@ -7,6 +7,7 @@ namespace SecurityLibrary.AES
     /// </summary>
     public class AES : CryptographicTechnique
     {
+        #region Fields
         private readonly byte[] sBox = new byte[]
         {
                 0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
@@ -26,7 +27,25 @@ namespace SecurityLibrary.AES
                 0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
                 0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16
         };
-
+        private readonly byte[] inverseSBox = new byte[]
+        {
+            0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+            0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+            0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+            0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+            0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+            0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+            0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+            0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+            0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+            0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+            0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+            0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+            0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+            0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+            0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+            0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D
+        };
         private readonly byte[] mixColumnsMatrix = new byte[16]
         {
             0x02, 0x03, 0x01, 0x01,
@@ -34,98 +53,163 @@ namespace SecurityLibrary.AES
             0x01, 0x01, 0x02, 0x03,
             0x03, 0x01, 0x01, 0x02
         };
-
+        private readonly byte[] inverseMixColumnsMatrix = new byte[16]
+        {
+            0x0E, 0x0B, 0x0D, 0x09,
+            0x09, 0x0E, 0x0B, 0x0D,
+            0x0D, 0x09, 0x0E, 0x0B,
+            0x0B, 0x0D, 0x09, 0x0E
+        };
         private string RconMatrix = "01000000020000000400000008000000100000002000000040000000800000001B00000036000000";
+        #endregion
 
+        #region Decryption
         public override string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            // Removing the "0x" prefix
+            cipherText = cipherText.Substring(2);
+            key = key.Substring(2);
+
+            // Previous key used in Key Schedule step
+            string previousKey = key;
+
+            // Getting all the keys
+            string[] currentKey = new string[10];
+            for (int i = 0; i < 10; i++)
+            {
+                currentKey[i] = KeySchedule(previousKey, i);
+                previousKey = currentKey[i];
+            }
+
+            // Initial Round
+            string state = AddRoundKey(cipherText, currentKey[9]);
+            string shiftRowsResult = InverseShiftRows(state);
+            string subBytesResult = InverseSubBytes(shiftRowsResult);
+            state = subBytesResult;
+
+            // Main Rounds
+            for (int i = 8; i >= 0; i--)
+            {
+                // AddRoundKey
+                string addRoundKeyResult = AddRoundKey(state, currentKey[i]);
+                // Mix Columns
+                string mixColumnsResult = InverseMixColumns(addRoundKeyResult);
+                // Shift Rows
+                shiftRowsResult = InverseShiftRows(mixColumnsResult);
+                // Substitute bytes
+                subBytesResult = InverseSubBytes(shiftRowsResult);
+                state = subBytesResult;
+            }
+
+            string plainText = AddRoundKey(state, key);
+
+            return "0x" + plainText;
         }
+        public string InverseSubBytes(string state)
+        {
+            byte[] cells = new byte[state.Length / 2];
+            for (int i = 0; i < state.Length / 2; i++)
+            {
+                cells[i] = Convert.ToByte(state.Substring(2 * i, 2), 16);
+            }
+
+            // Perform SubBytes operation
+            for (int i = 0; i < state.Length / 2; i++)
+            {
+                cells[i] = inverseSBox[cells[i]];
+            }
+
+            // Convert byte array back to string
+            string output = BitConverter.ToString(cells).Replace("-", "");
+
+            return output;
+        }
+        public string InverseShiftRows(string state)
+        {
+            state = Rotate(state);
+            string shiftedState = "";
+
+            for (int row = 0; row < 4; row++)
+            {
+                string rowState = state.Substring(row * 8, 8);
+                string shiftedRow = rowState.Substring((4 - row) * 2) + rowState.Substring(0, (4 - row) * 2);
+                shiftedState += shiftedRow;
+            }
+            shiftedState = Rotate(shiftedState);
+            return shiftedState;
+        }
+        public string InverseMixColumns(string state)
+        {
+            // Getting the MixColumns matrix 
+            string result = "";
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    byte a0 = Convert.ToByte(state.Substring(i * 8, 2), 16);
+                    byte a1 = Convert.ToByte(state.Substring(i * 8 + 2, 2), 16);
+                    byte a2 = Convert.ToByte(state.Substring(i * 8 + 4, 2), 16);
+                    byte a3 = Convert.ToByte(state.Substring(i * 8 + 6, 2), 16);
+
+                    byte multipliedCell = (byte)(GFMultiply(inverseMixColumnsMatrix[j * 4], a0) ^
+                                                 GFMultiply(inverseMixColumnsMatrix[j * 4 + 1], a1) ^
+                                                 GFMultiply(inverseMixColumnsMatrix[j * 4 + 2], a2) ^
+                                                 GFMultiply(inverseMixColumnsMatrix[j * 4 + 3], a3));
+                    result += multipliedCell.ToString("X2");
+                }
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region Encryption
         public override string Encrypt(string plainText, string key)
         {
             plainText = plainText.Substring(2);
             key = key.Substring(2);
-            // Rotating The matrix and the key
-            string newPlainText = "";
-            for (int i = 0; i < 8; i += 2)
-            {
-                newPlainText += plainText.Substring(i, 2);
-                newPlainText += plainText.Substring(i + 8, 2);
-                newPlainText += plainText.Substring(i + 16, 2);
-                newPlainText += plainText.Substring(i + 24, 2);
-            }
-            Console.WriteLine(newPlainText);
-
-            string newKey = "";
-            for (int i = 0; i < 8; i += 2)
-            {
-                newKey += key.Substring(i, 2);
-                newKey += key.Substring(i + 8, 2);
-                newKey += key.Substring(i + 16, 2);
-                newKey += key.Substring(i + 24, 2);
-            }
-            Console.WriteLine(newKey);
 
             // Previous key used in Key Schedule step
-            string previousKey = newKey;
+            string previousKey = key;
 
             // Initial Round
-            string state = AddRoundKey(newPlainText, newKey);
-            Console.WriteLine(state);
-            Console.WriteLine("-------------------------------");
+            string state = AddRoundKey(plainText, key);
 
             // Main Rounds
             for (int i = 0; i < 9; i++)
             {
                 // Substitute bytes
                 string subBytesResult = SubBytes(state);
-                Console.WriteLine(subBytesResult);
                 // Shift Rows
                 string shiftRowsResult = ShiftRows(subBytesResult);
-                Console.WriteLine(shiftRowsResult);
                 // Mix Columns
                 string mixColumnsResult = MixColumns(shiftRowsResult);
-                Console.WriteLine(mixColumnsResult);
                 // Key Scedule
                 string keySceduleResult = KeySchedule(previousKey, i);
                 previousKey = keySceduleResult;
-                Console.WriteLine(keySceduleResult);
                 // AddRoundKey
                 string addRoundKeyResult = AddRoundKey(mixColumnsResult, keySceduleResult);
                 state = addRoundKeyResult;
-                Console.WriteLine("-------------------------------");
-                Console.WriteLine(addRoundKeyResult);
             }
+
             // Final Round
             string finalRoundSubBytesResult = SubBytes(state);
-            Console.WriteLine(finalRoundSubBytesResult);
             string finalRoundshiftRowsResult = ShiftRows(finalRoundSubBytesResult);
-            Console.WriteLine(finalRoundshiftRowsResult);
             string finalRoundkeySceduleResult = KeySchedule(previousKey, 9);
-            Console.WriteLine(finalRoundkeySceduleResult);
-            string finalRoundAddRoundKeyResult = AddRoundKey(finalRoundshiftRowsResult, finalRoundkeySceduleResult);
-            Console.WriteLine(finalRoundAddRoundKeyResult);
+            string cipherText = AddRoundKey(finalRoundshiftRowsResult, finalRoundkeySceduleResult);
 
-            // Rotate the cipher text back
-            string cipherText = "";
-            for (int i = 0; i < 8; i += 2)
-            {
-                cipherText += finalRoundAddRoundKeyResult.Substring(i, 2);
-                cipherText += finalRoundAddRoundKeyResult.Substring(i + 8, 2);
-                cipherText += finalRoundAddRoundKeyResult.Substring(i + 16, 2);
-                cipherText += finalRoundAddRoundKeyResult.Substring(i + 24, 2);
-            }
             return "0x" + cipherText;
         }
-        private string AddRoundKey(string plainText, string cipherKey)
+        public string AddRoundKey(string state, string key)
         {
             string result = "";
-            for (int i = 0; i < plainText.Length; i += 2)
+            for (int i = 0; i < state.Length; i += 2)
             {
-                int plainTextCell = Convert.ToInt32(plainText.Substring(i, 2), 16);
-                int cipherKeyCell = Convert.ToInt32(cipherKey.Substring(i, 2), 16);
+                int stateCell = Convert.ToInt32(state.Substring(i, 2), 16);
+                int keyCell = Convert.ToInt32(key.Substring(i, 2), 16);
 
-                int xorResult = plainTextCell ^ cipherKeyCell;
+                int xorResult = stateCell ^ keyCell;
 
                 result += xorResult.ToString("X2");
             }
@@ -152,6 +236,7 @@ namespace SecurityLibrary.AES
         }
         private string ShiftRows(string state)
         {
+            state = Rotate(state);
             string shiftedState = "";
 
             for (int row = 0; row < 4; row++)
@@ -160,31 +245,74 @@ namespace SecurityLibrary.AES
                 string shiftedRow = rowState.Substring(row * 2) + rowState.Substring(0, row * 2);
                 shiftedState += shiftedRow;
             }
-
+            shiftedState = Rotate(shiftedState);
             return shiftedState;
         }
         private string MixColumns(string state)
         {
-            // Getting the MixColumns matrix by rows not columns like the lec
+            // Getting the MixColumns matrix 
             string result = "";
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
-                    byte a0 = Convert.ToByte(state.Substring(j * 2, 2), 16);
-                    byte a1 = Convert.ToByte(state.Substring(j * 2 + 8, 2), 16);
-                    byte a2 = Convert.ToByte(state.Substring(j * 2 + 16, 2), 16);
-                    byte a3 = Convert.ToByte(state.Substring(j * 2 + 24, 2), 16);
+                    byte a0 = Convert.ToByte(state.Substring(i * 8, 2), 16);
+                    byte a1 = Convert.ToByte(state.Substring(i * 8 + 2, 2), 16);
+                    byte a2 = Convert.ToByte(state.Substring(i * 8 + 4, 2), 16);
+                    byte a3 = Convert.ToByte(state.Substring(i * 8 + 6, 2), 16);
 
-                    byte multipliedCell = (byte)(GFMultiply(mixColumnsMatrix[0 + i * 4], a0) ^
-                                                    GFMultiply(mixColumnsMatrix[1 + i * 4], a1) ^
-                                                    GFMultiply(mixColumnsMatrix[2 + i * 4], a2) ^
-                                                    GFMultiply(mixColumnsMatrix[3 + i * 4], a3));
-
+                    byte multipliedCell = (byte)(GFMultiply(mixColumnsMatrix[j * 4], a0) ^
+                                                 GFMultiply(mixColumnsMatrix[j * 4 + 1], a1) ^
+                                                 GFMultiply(mixColumnsMatrix[j * 4 + 2], a2) ^
+                                                 GFMultiply(mixColumnsMatrix[j * 4 + 3], a3));
                     result += multipliedCell.ToString("X2");
                 }
             }
+
             return result;
+        }
+        private string KeySchedule(string key, int roundNumber)
+        {
+            // Getting each column alone
+            string c1 = key.Substring(0, 8),
+                   c2 = key.Substring(8, 8),
+                   c3 = key.Substring(16, 8),
+                   c4 = key.Substring(24, 8);
+
+            // Rotate the last column
+            string tmp = RotateWord(c4);
+            // Applying Subbytes to the last column
+            tmp = SubBytes(tmp);
+            // Doing XOR between first, last and racon columns
+            tmp = AddRoundKey(c1, tmp);
+            tmp = AddRoundKey(tmp, RconMatrix.Substring(roundNumber * 8, 8));
+            // Getting the new key
+            c1 = tmp;
+            c2 = AddRoundKey(c1, c2);
+            c3 = AddRoundKey(c2, c3);
+            c4 = AddRoundKey(c3, c4);
+
+            return c1 + c2 + c3 + c4;
+        }
+        #endregion
+
+        #region Helper Functions
+        private string Rotate(string matrix)
+        {
+            string r1 = "", r2 = "", r3 = "", r4 = "";
+            for (int i = 0; i < 32; i += 8)
+            {
+                r1 += (matrix[i]);
+                r1 += (matrix[i + 1]);
+                r2 += (matrix[i + 2]);
+                r2 += (matrix[i + 3]);
+                r3 += (matrix[i + 4]);
+                r3 += (matrix[i + 5]);
+                r4 += (matrix[i + 6]);
+                r4 += (matrix[i + 7]);
+            }
+            matrix = r1 + r2 + r3 + r4;
+            return matrix;
         }
         private byte GFMultiply(byte a, byte b)
         {
@@ -207,49 +335,11 @@ namespace SecurityLibrary.AES
             }
             return p;
         }
-        private string KeySchedule(string key, int roundNumber)
-        {
-            // 10000111
-            // 00101010
-            // 10101101
-            string c1 = "", c2 = "", c3 = "", c4 = "";
-            for (int i = 0; i < 32; i += 8)
-            {
-                c1 += (key[i]);
-                c1 += (key[i + 1]);
-                c2 += (key[i + 2]);
-                c2 += (key[i + 3]);
-                c3 += (key[i + 4]);
-                c3 += (key[i + 5]);
-                c4 += (key[i + 6]);
-                c4 += (key[i + 7]);
-            }
-            // Rotate the last column
-            string tmp = RotateWord(c4);
-            // Applying Subbytes to the last column
-            tmp = SubBytes(tmp);
-            // Doing XOR between first, last and racon columns
-            string tmp1 = AddRoundKey(c1, tmp);
-            tmp1 = AddRoundKey(tmp1, RconMatrix.Substring(roundNumber * 8, 8));
-            // Getting the new key
-            c1 = tmp1;
-            c2 = AddRoundKey(c1, c2);
-            c3 = AddRoundKey(c2, c3);
-            c4 = AddRoundKey(c3, c4);
-
-            string resultString = "";
-            for (int i = 0; i < c1.Length; i += 2)
-            {
-                resultString += c1.Substring(i, 2);
-                resultString += c2.Substring(i, 2);
-                resultString += c3.Substring(i, 2);
-                resultString += c4.Substring(i, 2);
-            }
-            return resultString;
-        }
         private string RotateWord(string key)
         {
             return key.Substring(2) + key[0] + key[1];
         }
+        #endregion
+
     }
 }
